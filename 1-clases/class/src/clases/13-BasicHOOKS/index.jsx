e32e1925f5cf0eb6d0f963434a724dbc4c06fef5
Freddy Huaylla <fmarcosdev@gmail.com>
2021-09-15 23:10:59 -0300

    ..
    ..

--- 1-clases/class/src/clases/13-BasicHOOKS/index.jsx
+++ 1-clases/class/src/clases/13-BasicHOOKS/index.jsx
@@ -8,3 +8,12 @@ function Index() {
 }
  
 export default Index;
+
+// useEfect se renderiza cada vez que actualizo algo en el componente (lifecycle) o cambio de algun estado con (setState) dentro del componente
+// useEfect [].    = render solo la primera vez 
+// useEfect return = render cuando temrina de cambiar el estado 
+
+
+
+
+ 
